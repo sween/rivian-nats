19d6323bada4c3c64f736e2690127baf02966c53
Ron Sweeney <ron.sweeney@integrationrequired.com>
2024-02-06 07:25:48 -0500

    changes

--- src/rivian-nats.py
+++ src/rivian-nats.py
@@ -6,11 +6,11 @@ async def main():
     # Connect to NATS!
     nc = await nats.connect("nats.deezwatts.com:42222")
 
-    # Receive messages on 'foo'
+    # Receive messages on 'deezwatts' dial tone
     sub = await nc.subscribe("deezwatts")
 
-    # Publish a message to 'foo'
-    # await nc.publish("foo", b'Hello from Python!')
+    # Publish a message to 'deezwatts'
+    # await nc.publish("deezwatts", b'Hello from Rivian!')
 
     # Process a message
     msg = await sub.next_msg()
@@ -18,7 +18,8 @@ async def main():
 
     # Speak to the Rivian
     os.system('echo %s | festival --tts' % msg)
-    # Close NATS connection
+
+    # Close NATS connection (Manners)
     await nc.close()
 
 if __name__ == '__main__':
